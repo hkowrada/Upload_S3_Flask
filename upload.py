4dc94bb4fc62cabfeb96cb67677a48193d314bea
Harish <40259552+hkowrada@users.noreply.github.com>
2020-07-24 03:19:17 +0530

    Update upload.py
    commit

--- upload.py
+++ upload.py
@@ -4,45 +4,44 @@ import boto3
 from config import S3_BUCKET, S3_KEY, S3_SECRET
 from filters import datetimeformat, file_type
 
-
-s3 = boto3.resource(
-    's3',
-    aws_access_key_id=S3_KEY, 
-    aws_secret_access_key=S3_SECRET)
+s3 = boto3.client(
+   "s3",
+   aws_access_key_id=S3_KEY,
+   aws_secret_access_key=S3_SECRET
+)
 
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = 'secret'
-app.jinja_env.filters['datetimeformat']=datetimeformat
-app.jinja_env.filters['file_type']=file_type
+app.jinja_env.filters['datetimeformat'] = datetimeformat
+app.jinja_env.filters['file_type'] = file_type
 
 
 @app.route('/')
 def index():
-        return render_template('index.html')
-        
+    return render_template("index.html")
+
 
 @app.route('/files')
 def files():
-        s3_resource = boto3.resource('s3')
-        my_bucket = s3_resource.Bucket(S3_BUCKET)
-        summaries = my_bucket.objects.all()
-    
-        return render_template('files.html', my_bucket=my_bucket, files=summaries)
+    s3_resource = boto3.resource('s3')
+    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    summaries = my_bucket.objects.all()
+
+    return render_template('files.html', my_bucket=my_bucket, files=summaries)
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-        file = request.files['file']
-        
-        s3_resource = boto3.resource('s3')
-        my_bucket = s3_resource.Bucket(S3_BUCKET)
-        my_bucket.Object(file.filename).put(Body=files)
-        
-        flash('File Uploaded successfully !!')
-        return redirect(url_for('files'))
-        
-    
+    file = request.files['file']
+
+    s3_resource = boto3.resource('s3')
+    my_bucket = s3_resource.Bucket(S3_BUCKET)
+    my_bucket.Object(file.filename).put(Body=file)
+
+    flash('File uploaded successfully')
+    return redirect(url_for('files'))
+
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
